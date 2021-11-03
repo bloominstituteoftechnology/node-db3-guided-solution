3682e47a6c7713998e8897b179c1187d83738469
Ladrillo <gabriel@cabrejas.net>
2021-11-03 13:45:15 +0100

    remove helmet, too many headers

--- api/server.js
+++ api/server.js
@@ -1,13 +1,10 @@
 const express = require('express')
-const helmet = require('helmet')
 
 const UserRouter = require('./users/user-router.js')
 
 const server = express()
 
-server.use(helmet())
 server.use(express.json())
-
 server.use('/api/users', UserRouter)
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({ message: err.message })

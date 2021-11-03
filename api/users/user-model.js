f9b193434f15ef97356eae554eed6525a87716b2
Ladrillo <gabriel@cabrejas.net>
2021-11-03 14:37:46 +0100

    add solution

--- api/users/user-model.js
+++ api/users/user-model.js
@@ -1,32 +1,66 @@
 const db = require('../../data/db-config.js')
 
 module.exports = {
+  findPosts,
   find,
   findById,
-  findPosts,
   add,
   update,
   remove
 }
 
-function find() {
-  return db('users')
-}
-
-function findById(id) {
-  // introduce first()
-  // we can return a single user object
-  // instead of an array
-  return db('users').where({ id }).first()
-}
-
-
-function findPosts(user_id) {
-  // copy code from GET /api/users/:id/posts
-  return db('posts as p')
+async function findPosts(user_id) {
+  const rows = await db('posts as p')
     .join('users as u', 'u.id', 'p.user_id')
     .select('p.id', 'u.username', 'p.contents')
     .where({ user_id })
+  return rows
+}
+
+async function find() {
+  const posts = await db('users as u')
+    .leftJoin('posts as p', 'u.id', 'p.user_id')
+    .select('u.id as user_id', 'u.username', 'p.id as post_id')
+  let users = []
+  for (let post of posts) {
+    let user = users.find(u => u.username === post.username)
+    if (user) {
+      user.post_count++
+    } else {
+      users.push({
+        user_id: post.user_id,
+        username: post.username,
+        post_count: post.post_id ? 1 : 0
+      })
+    }
+  }
+  return users
+}
+
+async function findById(id) {
+  const rows = await db('users as u')
+    .leftJoin('posts as p', 'u.id', 'p.user_id')
+    .select(
+      'u.id as user_id',
+      'u.username',
+      'p.id as post_id',
+      'p.contents',
+    )
+    .where('u.id', id)
+  let result = { posts: [] }
+  for (let record of rows) {
+    if (!result.username) {
+      result.user_id = record.user_id
+      result.username = record.username
+    }
+    if (record.post_id) {
+      result.posts.push({
+        contents: record.contents,
+        post_id: record.post_id,
+      })
+    }
+  }
+  return result
 }
 
 function add(user) {
@@ -38,8 +72,7 @@ function update(changes, id) {
   return db('users')
     .where({ id })
     .update(changes)
-    // eslint-disable-next-line no-unused-vars
-    .then(count => {
+    .then(count => { // eslint-disable-line
       return findById(id)
     })
 }

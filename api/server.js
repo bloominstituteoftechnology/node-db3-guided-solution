75c780c3fdcc7435acd7808a27aad1b8f1676a55
Ladrillo <gabriel@cabrejas.net>
2021-12-08 16:02:16 +0100

    checks

--- api/server.js
+++ api/server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 
-const UserRouter = require('./users/user-router.js')
+const userRouter = require('./users/user-router.js')
 
 const server = express()
 
 server.use(express.json())
-server.use('/api/users', UserRouter)
+server.use('/api/users', userRouter)
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({ message: err.message })
 })
